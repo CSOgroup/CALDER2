999b3485a2d74ee03ae3677cca6543489319ecf8
lucananni93 <luca.nanni@polimi.it>
2023-04-04 00:23:02 +0200

    improved cli testing

--- scripts/clean_test.sh
+++ scripts/clean_test.sh
@@ -4,12 +4,12 @@ source ~/miniconda3/etc/profile.d/conda.sh
 
 TEST_ENV_NAME="R-Test"
 
-mamba create --yes --name ${TEST_ENV_NAME} --channel bioconda \
-		r-essentials r-devtools bioconductor-biocinstaller bioconductor-genomicranges \
-		r-nloptr
+mamba create --yes --name ${TEST_ENV_NAME} --channel bioconda --channel conda-forge \
+		r-essentials r-devtools cooler r-nloptr
 conda activate ${TEST_ENV_NAME}
 
 R -e "devtools::install('.', dependencies=TRUE)"
+R -e "devtools::test()"
 
 conda deactivate 
 mamba env remove --yes --name ${TEST_ENV_NAME}
